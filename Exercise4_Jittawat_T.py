f9d01a1cabecb81247b711624bd269beacffae33
TorJT <jittawat.thi@gmail.com>
2022-01-25 20:34:56 +0700

    Exercise4_Jittawat_T
    Exercise 4 : ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ Exercise4_Jittawat_T.py
@@ -0,0 +1,15 @@
+'''Foundation English (FE)
+General Business (GB)
+Introduction to Computer Systems (ICS)
+Computer Programming (CP) '''
+
+FE_score = 68
+GB_score = 76
+ICS_score = 87
+CP_score = 92
+
+print("--- Your Score ---")
+print("Foundation English:", FE_score)
+print("General Business:", GB_score)
+print("Introduction to Computer Systems:", ICS_score)
+print("Computer Programming:", CP_score)
